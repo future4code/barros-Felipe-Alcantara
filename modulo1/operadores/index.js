8388fd06510524f505b6ef6f7b45eb21f5a48803
Felipe Alcântara <fmalcantara@hotmail.com>
2022-04-14 15:10:55 -0300

    Operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,104 @@
+// Exercicio de interpretação de código
+
+// Resumo das variaveis:
+// bool1 = verdadeiro
+// bool2 = falso
+// bool3 = verdadeiro
+
+// 1)
+// Resultado = bool1 && bool2 = false
+// resultado = bool1 && bool2 && bool3 = false
+// resultado = !resultado (bool1 || bool2) = verdadeiro
+
+// 2) Será impresso no console duas Strings em forma de numero nao ocorrendo as somas, apenas a junção das strings.
+// let primeiroNumero = prompt("Digite um numero!")
+// let segundoNumero = prompt("Digite outro numero!")
+
+// const soma = primeiroNumero + segundoNumero
+
+// console.log(soma)
+
+// 3) A solução seria transformar as strings de entrada em numeros utilizando o comando Number conforme abaixo.
+// let primeiroNumero = Number(prompt('Digite um numero!'))
+// let segundoNumero = Number(prompt('Digite outro numero!'))
+
+// const soma = primeiroNumero + segundoNumero
+
+// console.log(soma)
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>exercicio de escrita de codigo<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+/* 
+
+QUESTÃO 1
+
+let suaIdade = Number(prompt(' A) Qual a sua idade'))
+console.log(' A) Qual a sua idade', suaIdade)
+
+let idadeAmigo = Number(prompt('B) Qual a idade do seu melhor amigo ou amiga?'))
+console.log(' B) Qual a sua idade', idadeAmigo)
+
+let resultadoIdade = suaIdade > idadeAmigo
+
+console.log('C) Sua Idade é maior do que a do seu maior amigo?', resultadoIdade)
+
+let diferencaIdade = suaIdade - idadeAmigo
+console.log('A diferença de é de', diferencaIdade, 'anos') 
+
+================================================================================================================
+
+QUESTÃO 2
+
+A)
+
+let numeroPar = Number(prompt(' A) Insira um numero par'))
+console.log('voce digitou o numero', numeroPar)
+
+let divisao = numeroPar / 2
+console.log('O resultado da divisão é', divisao)
+
+let resto = numeroPar % 2
+
+console.log('o resto da divisão é', resto)
+
+
+B) O programa segue um padrão de resultado resto zero para quaisquer numero par e resto 1 para numero impar
+========================================================================================================================
+
+QUESTÃO 3
+ 
+let idadeAnos = Number(prompt('Qual sua idade em anos?'))
+
+a)
+let idadeMeses = idadeAnos * 12
+console.log('Sua idade em meses = ', idadeMeses)
+
+b)
+let idadeDias = idadeMeses * 30
+console.log('Sua idade em dias é', idadeDias)
+
+c)
+let idadeHoras = idadeDias * 24
+console.log('sSua idade em horas é', idadeHoras)
+
+===================================================================================================================
+
+QUESTÃO 4
+
+let number1 = Number(prompt('Digite um numero'))
+let number2 = Number(prompt('Digite outro Numero'))
+
+let resultadoMaior = number1 > number2
+console.log('O primeiro é Maior que o segundo', resultadoMaior)
+
+let resultadoIgual = number1 === number2
+console.log('O primeiro é igual ao segundo?', resultadoIgual)
+
+let divisivel1 = number1 % number2 === 0
+console.log('O primeiro é divisível pelo segundo?', divisivel1)
+
+let divisivel2 = number2 % number1 === 0
+console.log('O segundo é divisivel pelo primeiro?', divisivel2)
+
+
+
+*/
