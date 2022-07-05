b703dcee362610cf6789170485517e9eac162b16
felipe_alcantara <fmalcantara@hotmail.com>
2022-07-05 10:44:35 -0300

    Atualizar

--- modulo3/insta4/src/components/SecaoComentario/SecaoComentario.js
+++ modulo3/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -5,10 +5,23 @@ export function SecaoComentario(props) {
 
   function onChangeComentario(write) {
     console.log(write)
+
+    
+
+  function handleAddpostagem() {
+    const newPost = { 
+      nome: props.momeUsuario,
+      foto: props.fotousuario,
+      fotoPost: props.fotoPost
+    }
+    console.log (newPost)
+  }
+
 	}
   return (
     <div className="CommentContainer">
       <input
+        onCLick={handleAddpostagem}
         className="InputComentario"
         placeholder={'Comentário'}
 				onChange={ e => onChangeComentario(e.target.value)}
