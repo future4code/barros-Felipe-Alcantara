779bc89ae79813c4222268ed932c7d554b6ed466
felipe_alcantara <fmalcantara@hotmail.com>
2022-07-05 18:46:51 -0300

    comit

--- modulo3/styled-components/ex-styled-components/src/App.js
+++ modulo3/styled-components/ex-styled-components/src/App.js
@@ -1,37 +1,64 @@
-
-import {Container} from './style.js'
-import {Cabecalho} from './style.js'
-import {ColunaDireita} from './style.js'
-import {ColunaEsquerda} from './style.js'
-import {Centro} from './style.js'
-import {Rodape} from './style.js'
+import { Container } from './style.js'
+import { Cabecalho } from './style.js'
+import { ColunaDireita } from './style.js'
+import { ColunaEsquerda } from './style.js'
+import { Centro } from './style.js'
+import { Rodape } from './style.js'
 import logo from './logo.png'
+import { useState } from 'react'
+import Card from './Components/Card.jsx'
 
 function App() {
+// estado criado para digitar no imput e guardar a mensagem qye acabou de ser digitada.
+  const [escrito, setEscrito] = useState('')
 
-  return ( 
+// estado criado para guardar a mensagem fixada e incementar pelo metdodo .map outras mensagens. 
+  const [msgFixada, setMsgFixada] = useState([])
+
+  function handleAddMsg() {
+    const newMsg = {
+      msg: escrito,
+      horario: new Date().toLocaleDateString('pt-br', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      })
+    }
+    setMsgFixada(prevState => [...prevState, newMsg])
+  }
+
+  return (
     <Container>
-        
-        <Cabecalho> 
-          <img src={logo}/>
-        </Cabecalho>
-    
-          <Centro>
-            <ColunaEsquerda>  </ColunaEsquerda>
+      <Cabecalho>
+        <img src={logo} />
+      </Cabecalho>
 
-            <span> remetente </span> <input type="text"/> 
-            <span>MSG </span> <input type="text"/>
-            <button>Enviar Mensagem</button> 
+      <Centro>
+        <ColunaEsquerda> </ColunaEsquerda>
+        <span> Remetente </span>
+        <input type="text" placeholder="Digite o remetente..." />
 
-            <ColunaDireita> </ColunaDireita> 
-          </Centro>
-         
-      <Rodape>Copright 2022 Felipe Alcantara All rights reserved , Rua tal do Sanfoneiro 2022</Rodape>
-      
-      </Container>
-    )
+        <span> Mensagem </span>
+        <input
+          type="text"
+          placeholder="Digite sua msg..."
+          onChange={e => setEscrito(e.target.value)}
+        />
 
-  
- }
+        {msgFixada.map(mssg => (
+          <Card mensagem={mssg.msg} hora={mssg.horario} />))
+        }
+
+        <button onClick={handleAddMsg}> Enviar Mensagem</button>
+        <ColunaDireita> </ColunaDireita>
+      </Centro>
+
+      <Rodape>
+        Copright 2022 Felipe Alcantara All rights reserved , Rua tal do
+        Sanfoneiro 2022
+      </Rodape>
+    </Container>
+  )
+}
 
 export default App
---
+++ modulo3/styled-components/ex-styled-components/src/Components/Card.jsx
@@ -0,0 +1,11 @@
+function Card(props){
+  return(
+    <>
+    <small>{props.mensagem}</small>
+    <small>{props.hora}</small>
+    
+    </>
+  )
+}
+
+export default Card
