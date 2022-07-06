2bb210c46ab8f83ace4634e63fd7aca3f257a53d
felipe_alcantara <fmalcantara@hotmail.com>
2022-07-05 23:42:45 -0300

    Exercicio insta

--- modulo3/insta4/src/App.js
+++ modulo3/insta4/src/App.js
@@ -4,9 +4,18 @@ import './style.css'
 
 
 function App() {
+
+  const postagem = [
+    {nome: "Paulinha", imagem: "https://picsum.photos/50/55", pic: "https://picsum.photos/40/55" },
+    {nome: "Antonio", imagem: "https://picsum.photos/50/55", pic: "https://picsum.photos/210/55" },
+    {nome: "Jose", imagem: "https://picsum.photos/9/50", pic: "https://picsum.photos/206/55" },
+  ]
+
+    const postagemMap=postagem.map( (item)=> {return <Post nomes={item.nome} foto={item.imagem} pic={item.pic}/>})
+
 return(
   <div>
-    
+      {postagemMap}
   </div>
 )
 
