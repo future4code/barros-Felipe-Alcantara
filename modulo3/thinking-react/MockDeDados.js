00665158929f7d22aff0d656171fbd0134a2a281
felipe_alcantara <fmalcantara@hotmail.com>
2022-07-14 21:15:46 -0300

    Exercicio Thinking in React

---
+++ modulo3/thinking-react/MockDeDados.js
@@ -0,0 +1,49 @@
+const produto= [
+
+{
+  id: 1,
+  name: 'Produto legal',
+  price: 50,
+  photo: 'https://picsum.photos/200/300',
+},
+
+
+{
+  id: 2,
+  name: 'Produto barato',
+  price: 10,
+  photo: 'https://picsum.photos/200/300',
+},
+
+{
+  id: 3,
+  name: 'Produto caro',
+  price: 200,
+  photo: 'https://picsum.photos/200/300',
+},
+
+{
+  id: 4,
+  name: 'Produto essencial',
+  price: 70,
+  photo: 'https://picsum.photos/200/300',
+
+},
+
+{
+  id: 5,
+  name: 'Poduto necesario',
+  price: 80,
+  photo: 'https://picsum.photos/200/300',
+},
+
+
+{
+  id: 6,
+  name: 'Poduto ocasional',
+  price: 20,
+  photo: 'https://picsum.photos/200/300',
+},
+
+
+]
